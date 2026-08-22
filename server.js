e881daa13638abc2ef184dca0a0461e3debc8b2e
Geovanne Rabelo <georabelo11@gmail.com>
2026-08-22 09:17:12 -0300

    feat: adicionar tratamento de excecao

--- server.js
+++ server.js
@@ -12,9 +12,22 @@ const users = [{
 }];
 
 http.createServer((request, response) => {
+    if (request.url !== '/users') {
+        response.writeHead(
+            404,
+            { 'content-type': 'application/json' }
+        );
+        response.end(JSON.stringify({ 
+            error: 404,
+            message: 'Resource not found!' 
+        })); // transforma array em JSON e .end() mostra na tela a resposta
+        return;
+    }
+
     response.writeHead(
         200, 
         { "content-type": "application/json" }
     );
     response.end(JSON.stringify(users)); // transforma array em JSON e .end() mostra na tela a resposta
+
 }).listen(3000);
